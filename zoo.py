5ae8ad7d91eda0e7a80a65c50a9fe9d4047a59dd
astroud13 <113217155+astroud13@users.noreply.github.com>
2024-04-09 14:45:22 -0400

    Add files via upload

---
+++ zoo.py
@@ -0,0 +1,11 @@
+#Module 4 Programming Assignment - Modules and Lists
+#By: Austin Stroud
+#File name: zoo.py 
+#Description: 11.1 part 1
+
+#11.1 part 1
+#Define function called hours()
+def hours():
+    #Print string 'Open 9-5 daily'
+    print('Open 9-5 daily')
+
